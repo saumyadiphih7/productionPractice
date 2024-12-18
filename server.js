af7c1f606148414519e665669726dd71385929e9
saumyadiphih7 <saumyadip@hih7.com>
2024-12-18 10:34:30 +0530

    add env file

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import 'dotenv/config'
+
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -72,8 +75,8 @@ app.delete("/delete/name/:id", (req, res) => {
 })
 
 
-const port=3000;
+
 
 app.listen(port, () =>
-  console.log("Server is running on port 3000")
+  console.log(`server is running on port ${port}`)
 );
